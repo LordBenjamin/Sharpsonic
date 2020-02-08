e9135299ae1e2303ce92f217772956f8267a3977
Ben Owen <LordBenjamin@users.noreply.github.com>
2020-02-08 21:24:38 +0000

    Format serializer output similar to Subsonic, makes Fiddler comparisons a lot easier.

--- src/Api/Serialization/CustomXmlSerializerOutputFormatter.cs
+++ src/Api/Serialization/CustomXmlSerializerOutputFormatter.cs
@@ -1,17 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
 
 namespace Api.Serialization {
     public class CustomXmlSerializerOutputFormatter : XmlSerializerOutputFormatter {
         public CustomXmlSerializerOutputFormatter()
-            : base() {
+            : base(new XmlWriterSettings {
+                OmitXmlDeclaration =false,
+#if DEBUG
+                Indent = true,
+                NewLineHandling = NewLineHandling.Entitize,
+#endif
+            }) {
         }
 
         public CustomXmlSerializerOutputFormatter(ILoggerFactory loggerFactory)
