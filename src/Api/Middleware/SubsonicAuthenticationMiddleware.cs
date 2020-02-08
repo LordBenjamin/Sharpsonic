20582bdef1209f1028b4fe25f8642e76b32696ce
Ben Owen <LordBenjamin@users.noreply.github.com>
2020-02-08 23:48:39 +0000

    Update authentication middleware to handle the dreaded plaintext password case.

--- src/Api/Middleware/SubsonicAuthenticationMiddleware.cs
+++ src/Api/Middleware/SubsonicAuthenticationMiddleware.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 using System;
 using System.Security.Claims;
@@ -22,27 +22,61 @@ namespace Api.Middleware {
             string userName = GetQueryParameter(context, "u");
             string token = GetQueryParameter(context, "t");
             string salt = GetQueryParameter(context, "s");
+            string password = GetQueryParameter(context, "p");
 
-            bool isAuthenticated =
-                AreEquivalent(context, options.UserName, userName) &&
-                AreEquivalent(context, Hash(options.Password, salt), token);
-
-            if (isAuthenticated) {
-                Claim[] claims = new[] {
-                    new Claim("name", userName),
-                    new Claim(ClaimTypes.Role, "User"),
-                };
-
-                ClaimsIdentity identity = new ClaimsIdentity(claims, "Basic");
-                context.User = new ClaimsPrincipal(identity);
-            }
-            else {
-                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            if (!AreEquivalent(context, options.UserName, userName)) {
+                await WriteAuthFailure(context);
                 return;
             }
 
+            bool passwordAccepted;
+
+            if (!string.IsNullOrWhiteSpace(token) && !string.IsNullOrWhiteSpace(salt)) {
+                passwordAccepted = AreEquivalent(context, Hash(options.Password, salt), token);
+            }
+            else {
+                passwordAccepted = string.Equals(
+                    options.Password, DecodePlaintextPassword(password), StringComparison.Ordinal);
+            }
+
+
+
+            if (!passwordAccepted) {
+                await WriteAuthFailure(context);
+                return;
+
+            }
+
+            Claim[] claims = new[] {
+                new Claim("name", userName),
+                new Claim(ClaimTypes.Role, "User"),
+            };
+
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "Basic");
+            context.User = new ClaimsPrincipal(identity);
+
             // Call the next delegate/middleware in the pipeline
             await _next(context);
+        }
+
+        // TODO: What if a user's password legitimately stars with "enc:"??
+        private string DecodePlaintextPassword(string password) {
+            if (!password.StartsWith("enc:", StringComparison.OrdinalIgnoreCase)) {
+                return password; // Totally plaintext
+            }
+
+            return Encoding.UTF8.GetString(HexStringToBytes(password.Substring(4)));
+        }
+
+        private async Task WriteAuthFailure(HttpContext context) {
+            Version version = SubsonicApiVersioningMiddleware.ServerVersion;
+
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsync(
+"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+$"<subsonic-response xmlns=\"http://subsonic.org/restapi\" status=\"failed\" version=\"{version.ToString(3)}\">\n" +
+"   <error code=\"40\" message=\"Wrong username or password.\"/>\n" +
+"</subsonic-response>");
         }
 
         private static string GetQueryParameter(HttpContext context, string parameterName) {
@@ -66,6 +100,20 @@ namespace Api.Middleware {
             }
 
             return sb.ToString();
+        }
+
+        // https://codereview.stackexchange.com/a/97971
+        public static byte[] HexStringToBytes(string hexString) {
+            if (hexString == null)
+                throw new ArgumentNullException("hexString");
+            if (hexString.Length % 2 != 0)
+                throw new ArgumentException("hexString must have an even length", "hexString");
+            var bytes = new byte[hexString.Length / 2];
+            for (int i = 0; i < bytes.Length; i++) {
+                string currentHex = hexString.Substring(i * 2, 2);
+                bytes[i] = Convert.ToByte(currentHex, 16);
+            }
+            return bytes;
         }
     }
 }
