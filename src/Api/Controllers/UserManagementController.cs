62e7477b2280c98913db2c1d64bb3f1a1114f20d
Ben Owen <LordBenjamin@users.noreply.github.com>
2020-02-09 15:24:51 +0000

    Implement basic UserManagementController. Deny permissions for functionality not implemented yet
    N.B. DSub doesn't seem to respect the downloadRole setting and the button is still visible - although it also doesn't matter that it isn't implemented, because it seems to use the Stream endpoint anyway!

---
+++ src/Api/Controllers/UserManagementController.cs
@@ -0,0 +1,60 @@
+using Api.DataTransfer;
+using Api.Media;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Linq;
+
+namespace Api.Controllers {
+    [Route("rest")]
+    [ApiController]
+    [Produces("application/xml")]
+    public class UserManagementController : ControllerBase {
+        public UserManagementController(IOptions<ApplicationSettings> appSettings, MediaIndex index) {
+            Settings = appSettings.Value;
+            Index = index;
+        }
+
+        public ApplicationSettings Settings { get; }
+        public MediaIndex Index { get; }
+
+        [HttpGet]
+        [Route("getUser")]
+        [Route("getUser.view")]
+        public ActionResult<Response> GetUser(string userName) {
+            if (!string.Equals(Settings.UserName, userName, System.StringComparison.OrdinalIgnoreCase)) {
+                return new Response {
+                    Item = new Error {
+                        code = 50, // Not authorized
+                        message = "User is not authorized for the given operation.",
+                    }
+                };
+            }
+
+            // TODO: Enable these as features are added. Eventually implement actual permissions.
+            return new Response {
+                ItemElementName = ItemChoiceType.user,
+                Item = new User {
+                    username = userName,
+                    adminRole = false,
+                    commentRole = false,
+                    downloadRole = false,
+                    coverArtRole = true,
+                    jukeboxRole = false,
+                    playlistRole =false,
+                    podcastRole = false,
+                    scrobblingEnabled = false,
+                    settingsRole = false,
+                    shareRole = false,
+                    streamRole = true,
+                    uploadRole = false,
+                    videoConversionRole = false,
+
+                    folder = Index.Entries
+                        .Where(i => i.IsFolder && i.ParentId == -1)
+                        .Select(i => i.Id)
+                        .ToArray(),
+                }
+            };
+        }
+    }
+}
