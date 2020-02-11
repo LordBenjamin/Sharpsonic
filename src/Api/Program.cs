da735d2b3805aea14fc27526559ef14ddd7192fe
Ben Owen <LordBenjamin@users.noreply.github.com>
2020-02-11 23:15:10 +0000

    Recreate config file from the default one if nothing found in the /config directory.

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace Sharpsonic.Api {
     public class Program {
@@ -11,9 +12,15 @@ namespace Sharpsonic.Api {
 
         public static IHostBuilder CreateHostBuilder(string[] args) => Host
             .CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((buildercontext, config) => {
-                    config.AddJsonFile("/config/appsettings.json", optional: true);
-                })
+            .ConfigureAppConfiguration((buildercontext, config) => {
+                // TODO: Hardcoded paths - can we pick this up from the environment?
+                // E.g. what happens on a Windows container?
+                if (!File.Exists("/config/appsettings.json")) {
+                    File.Copy("appsettings.json", "/config/appsettings.json");
+                }
+
+                config.AddJsonFile("/config/appsettings.json", optional: true);
+            })
             .ConfigureWebHostDefaults(webBuilder => {
                 webBuilder.UseStartup<Startup>();
             });
