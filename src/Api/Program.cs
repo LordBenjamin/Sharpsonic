065f6264f54a1bc0d093eb7fec180aef1d8cab6f
Ben Owen <LordBenjamin@users.noreply.github.com>
2020-02-12 18:45:57 +0000

    Fix (or more likely hack around) issue with mounting the config file as a directory.

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -15,7 +15,7 @@ namespace Sharpsonic.Api {
             .ConfigureAppConfiguration((buildercontext, config) => {
                 // TODO: Hardcoded paths - can we pick this up from the environment?
                 // E.g. what happens on a Windows container?
-                if (!File.Exists("/config/appsettings.json")) {
+                if (!File.Exists("/config/appsettings.json") && Directory.Exists("/config")) {
                     File.Copy("appsettings.json", "/config/appsettings.json");
                 }
 
