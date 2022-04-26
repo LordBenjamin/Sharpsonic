767116f973b773cc74721d7de80b3c00f599c860
Ben Owen <LordBenjamin@users.noreply.github.com>
2022-04-26 21:39:07 +0100

    Set default namespace / assembly name

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Client;
 using Microsoft.Extensions.DependencyInjection;
 using System.Net.Http;
 using System;
+using Auricular.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
